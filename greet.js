f0dd8c7558a12aa1b5ca83fb7993235799e05981
Mbalenh <3408534@myuwc.ac.za>
2022-07-18 17:56:24 +0200

    added modifield files

--- greet.js
+++ greet.js
@@ -1,4 +1,3 @@
-module.exports = function(name){
+export default function greet(name){
   return "hello " + name;
 }
-
--- greet_xola.js
+++ greet_xola.js
@@ -1,5 +1,17 @@
-var chalk = require('chalk');
-var greet = require('./greet');
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
+// CommonJS:
+import chalk from "chalk"
+import figlet from "figlet"
+import greet from "./greet.js"
+// const styledMessage = chalk.bgGreen.black(greet('Xola'));
 // console.log(greet('Xola'))
-console.log(styledMessage)
+figlet(greet('Xola'), function(err, data) {
+    "use strict";
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        // return;
+    }
+  
+    const styledMessage = chalk.bgGreen.black(data);
+    console.log(styledMessage);
+  });
